9c2de221f7f589e4b4349402db019c4bf520f309
Ridish008 <ridishravi@gamil.com>
2024-02-07 11:53:05 +0530

    first commit

---
+++ app/src/main/java/com/example/weatherbuddy/LoginForm.java
@@ -0,0 +1,35 @@
+package com.example.weatherbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginForm extends AppCompatActivity {
+    TextView registration, login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+        registration = findViewById(R.id.newAccount);
+        login = findViewById(R.id.login);
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginForm.this, RegistrationForm.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginForm.this, healthInfo.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
